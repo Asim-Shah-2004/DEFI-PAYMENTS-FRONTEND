d52220c6ba4f45db78fdc0432fdaab2fb83302a0
Asim Shah <asimshah2111@gmail.com>
2024-04-08 03:59:52 +0530

    added payments without using contracts

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,82 @@
 import { useState } from "react";
-import {ethers} from "ethers";
+import { ethers } from "ethers";
+import ErrorMessage from "./ErrorMessage";
+import TxList from "./TxList";
 
-async function connectWallet(setWalletAddress) {
-  if (typeof window.ethereum !== 'undefined') {
-    await requestAccounts(setWalletAddress);
-    const provider = new ethers.BrowserProvider(window.ethereum)
-    console.log(provider);
+const startPayment = async ({ setError, setTxs, ether, addr }) => {
+  try {
+    if (!window.ethereum)
+      throw new Error("No crypto wallet found. Please install it.");
+
+    await window.ethereum.send("eth_requestAccounts");
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    ethers.utils.getAddress(addr);
+    const tx = await signer.sendTransaction({
+      to: addr,
+      value: ethers.utils.parseEther(ether)
+    });
+    console.log({ ether, addr });
+    console.log("tx", tx);
+    setTxs([tx]);
+  } catch (err) {
+    setError(err.message);
   }
-}
+};
 
-async function requestAccounts(setWalletAddress) {
-  console.log("working");
-  if (window.ethereum) {
-    console.log("detected");
+export default function App() {
+  const [error, setError] = useState();
+  const [txs, setTxs] = useState([]);
 
-    try {
-      const accounts = await window.ethereum.request({ method: "eth_requestAccounts" });
-      console.log(accounts);
-      setWalletAddress(accounts[0]);
-    } catch (error) {
-      console.log(error);
-    }
-  } else {
-    console.log("please install metamask");
-  }
-}
-
-const App = () => {
-  const [walletAddress, setWalletAddress] = useState("");
-
-  const handleConnectWallet = () => {
-    connectWallet(setWalletAddress);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const data = new FormData(e.target);
+    setError();
+    await startPayment({
+      setError,
+      setTxs,
+      ether: data.get("ether"),
+      addr: data.get("addr")
+    });
   };
 
   return (
-    <>
-      <button onClick={handleConnectWallet}>Connect to wallet</button>
-      <div>
-        <h3>Wallet Address: {walletAddress}</h3>
+    <form className="m-4" onSubmit={handleSubmit}>
+      <div className="credit-card w-full lg:w-1/2 sm:w-auto shadow-lg mx-auto rounded-xl bg-white">
+        <main className="mt-4 p-4">
+          <h1 className="text-xl font-semibold text-gray-700 text-center">
+            Send ETH payment
+          </h1>
+          <div className="">
+            <div className="my-3">
+              <input
+                type="text"
+                name="addr"
+                className="input input-bordered block w-full focus:ring focus:outline-none"
+                placeholder="Recipient Address"
+              />
+            </div>
+            <div className="my-3">
+              <input
+                name="ether"
+                type="text"
+                className="input input-bordered block w-full focus:ring focus:outline-none"
+                placeholder="Amount in ETH"
+              />
+            </div>
+          </div>
+        </main>
+        <footer className="p-4">
+          <button
+            type="submit"
+            className="btn btn-primary submit-button focus:ring focus:outline-none w-full"
+          >
+            Pay now
+          </button>
+          <ErrorMessage message={error} />
+          <TxList txs={txs} />
+        </footer>
       </div>
-    </>
+    </form>
   );
-};
-
-export default App;
+}
